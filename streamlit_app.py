3a5ee5ebb0ee410501f3dfd296e5c5ec67de3d7e
Brandon Fangio <114620495+BrandonFangio@users.noreply.github.com>
2023-08-18 15:15:12 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 import pandas
 import requests
-from snowflake-connector-python import snowflake.connector
+import snowflake.connector
 
 streamlit.title("My Parents New Healthy Diner")
 
