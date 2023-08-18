a2b5e82d434aa59edb8310c6e3e24ffb977ffb57
Brandon Fangio <114620495+BrandonFangio@users.noreply.github.com>
2023-08-18 15:37:30 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,6 +41,8 @@ fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_cho
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
+
+streamlit.stop()
 add_my_fruit = fruit_choice
 streamlit.write('thanks for adding', add_my_fruit)
 
